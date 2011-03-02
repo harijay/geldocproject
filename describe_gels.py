6e2c1ebf893191c980e657afb983f1d39d182002
Hari Jayaram <harijay@gmail.com>
2011-03-02 16:06:00 -0500

    Molecular weight labels added

--- describe_gels.py
+++ describe_gels.py
@@ -12,8 +12,31 @@ from optparse import OptionParser
 p = OptionParser()
 p.add_option("-d" , "--dir",dest="tif_source_dir",metavar="tif source_directory")
 global_options, spillover = None,None
-p.add_option("-i","--image",dest="pngimage",metavar="single run image")
+p.add_option("-i","--image",dest="pngimage",metavar="file.png", help="image for 16 bit png to 8 bit png")
+p.add_option("-s","--single",dest="singletif",metavar="single tif image")
 
+def make_rgb8from_16bit_tif(filename):
+    try:
+        print "Processing {afile}".format(afile=filename)
+        im = Image.open(filename)
+        dpi = im.info["dpi"]
+        print "Input mode:" , im.mode
+        print "Input size:", im.size
+        print "Input  dpi:" ,dpi
+        im2 =  im.convert("I")
+        print "Output mode1:",im2.mode
+      # out = im2.resize([int(0.5 *s) for s in im2.size],Image.ANTIALIAS)
+        out = im2.resize([800,600],Image.ANTIALIAS)
+        out.save(os.path.splitext(filename)[0] + "_proc.png")
+        png_object = png.Reader(os.path.splitext(filename)[0] + "_proc.png")
+        png_object_writer = png.Writer(width=800,height=600,bitdepth=8)
+        outfile_fin = open((os.path.splitext(filename)[0] + "rgb8.png"),"wb")
+        png_object_writer.write(outfile_fin,png_object.asRGB8()[2])
+        outfile_fin.close()
+    except Exception as e:
+        print "Error during rgba8 from tif",e
+        
+    
 if __name__=="__main__" :
     if len(sys.argv) == 1:
         p.print_help()
@@ -56,4 +79,8 @@ if __name__=="__main__" :
             outfile_fin.close()
         except Exception as e:
             e
+
+    if global_options.singletif is not None:
+        make_rgb8from_16bit_tif(global_options.singletif)
+        
 
--- report.py
+++ report.py
@@ -3,28 +3,33 @@ import reportlab
 from reportlab.pdfgen import canvas
 from reportlab.lib import pagesizes
 from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch,mm
+from reportlab.lib.units import inch,mm,cm
 from optparse import OptionParser
 import sys
+import os
+import subprocess
+import math
 
-
-
-def report_image_as_pdf(imagename,left_start,hspread,lane_vert_pos,num_lanes,lanenames,flip):
+def report_image_as_pdf(imagename,left_start,hspread,lane_vert_pos,num_lanes,lanenames,flip,show_grid,bench_mark_x,fudge,shift_marker,scale_marker):
     image_name = imagename
     lanenames = lanenames
     left_start = float(left_start)
     num_lanes = int(num_lanes)
     hspread = float(hspread)
     lane_vert_pos = float(lane_vert_pos)
-    
-    print lanenames
-    c = canvas.Canvas("hello{image_name}.pdf".format(image_name=image_name),pagesize=pagesizes.landscape(letter))
-#    for x in range(0,11,1):
-#        for y in range(0,9,1): 
-#            c.drawString(x*inch,y*inch,"%d,%d" % (x,y))
-            
-    lane_label_xspread = (7 -(hspread))/num_lanes
-    
+    image_name = os.path.splitext(imagename)[0]
+    bench_mark_x = float(bench_mark_x)
+    fudge = list(fudge)
+    shift_marker = float(shift_marker)
+    scale_marker = float(scale_marker)
+    c = canvas.Canvas("{image_name}_report.pdf".format(image_name=image_name),pagesize=pagesizes.landscape(letter))
+    c.setTitle("{image_name}_report.pdf".format(image_name=image_name))     
+
+    if show_grid:
+        for lx in range(11):
+            for ly in range(9):
+                c.drawString(lx*inch,ly*inch, "%d,%d" % (lx,ly))
+                
     if flip:
         c.translate(11*inch, 0)
         c.scale(-1.0, 1.0)
@@ -37,26 +42,35 @@ def report_image_as_pdf(imagename,left_start,hspread,lane_vert_pos,num_lanes,lan
         c.drawImage("{image_name}.png".format(image_name=image_name),2*inch,2.5*inch,width=7*inch,height=5*inch)
         
     for lane_number in range(1,num_lanes + 1, 1):
-        c.drawString((left_start + lane_number * lane_label_xspread)*inch,lane_vert_pos*inch,u"%s" % (str(lane_number)))
+        c.drawString((left_start + lane_number * hspread)*inch,lane_vert_pos*inch,u"%s" % (str(lane_number)))
 
+    # Putting the legend on the canvas
     for i in range(num_lanes):
         left_pos_x = 2*inch
         laneinfo_spread = 2.0*inch/(num_lanes/2)
-        print laneinfo_spread
         lane_index = i
         if i >=  num_lanes/2:
             left_pos_x = 5.0*inch
             i = i - num_lanes/2
-            
         try:
-            print "Lane infor for", i
-            print "LEFT",left_pos_x ,"RIGHT", 2 - i*laneinfo_spread
             c.drawString(left_pos_x , (2*inch - i*laneinfo_spread),u"Lane %d : %s" % (lane_index+1 , lanenames[lane_index].strip()))
         except IndexError:
-            print "ERREUEUEU"
+            print "Too many lanes in lane names array"
             
+
+    bench_mark_y_positions =[6.6,6.45,6.23,6.00,5.50,5.0,4.2,3.9]
+    bench_mark_values=[200,150,100,75,50,37,25,20]
+    c.setFont('Helvetica', 6)
+    for i,j in enumerate(bench_mark_values):
+        xpos = bench_mark_x
+        #ypos = (math.log10(j*1e3) - 3.333)/0.11
+        ypos = bench_mark_y_positions[i]
+        print "YPOS %d" % j , ypos
+        c.drawString(bench_mark_x*inch,(shift_marker+ypos)*scale_marker*inch,"%s" % j)
+        
     c.showPage()
     c.save()
+    subprocess.call(["evince" , "{image_name}_report.pdf".format(image_name=image_name)])
     
 
 if __name__=="__main__":
@@ -64,12 +78,17 @@ if __name__=="__main__":
         
     p = OptionParser()
     p.add_option("--png" ,"-p", dest="pngimage", metavar = "*.png", help="png image to annotate")
-    p.add_option("--hspread","-w", dest="hspread",help="distance between lane labels default 1.52 inches",default=1.52)
-    p.add_option("--numlanes", "-n", dest="numlanes",metavar="Number of lanes",default=12)
-    p.add_option("--lanenames","-s",dest="lanenames",help="lane names as csv",default=["Lane %s Label" % i for i in range(1,13,1)])
-    p.add_option("--left_start" , "-b" , dest="left_start", help="gap before labels", default = 2.12)
+    p.add_option("--gap","-g", dest="hspread",help="The gap between gel lanes",metavar="0.46",default=0.46)
+    p.add_option("--numlanes", "-n", dest="numlanes",metavar="12",default=12)
+    p.add_option("--legend","-l",dest="lanenames",help="lane names as csv",default=",".join([("Lane %s Label") % i for i in range(1,12,1)]))
+    p.add_option("--margin" , "-m" , dest="left_start", help="left hand margin",metavar="2.12", default = 2.12)
     p.add_option("--flip","-f",action="store_true",dest ="flip")
-    p.add_option("--top_pos" , "-t",dest="lane_vert_pos",metavar="Lane vert pos[6.7]",default=6.7)
+    p.add_option("--top_pos" , "-t",dest="lane_vert_pos",help="lanelabel pos",metavar="6.7",default=6.7)
+    p.add_option("--show_grid" ,"-r", dest="show_grid",help="overlay grid for planning layout" , action="store_true")
+    p.add_option("--bench_mark_x" , "-b" , dest="bench_mark_x")
+    p.add_option("--fudge","-u",dest="fudge", metavar="[6.6,6.45,6.23,6.00,5.50,5.0,4.2,3.9]",default="[6.6,6.45,6.23,6.00,5.50,5.0,4.2,3.9]",help="fudge factor for mwt labels")
+    p.add_option("--shift_marker",dest="shift_marker", default=0, metavar="0")
+    p.add_option("--scale_marker", dest="scale_marker", default=1 , metavar="0")
     options,args = None,None
     if (len(sys.argv) == 1):
         p.print_help()
@@ -77,7 +96,7 @@ if __name__=="__main__":
     try:
         options,args = p.parse_args()
         print options
-        report_image_as_pdf(options.pngimage ,options.left_start,options.hspread,options.lane_vert_pos,options.numlanes,options.lanenames.split(","),options.flip)
+        report_image_as_pdf(options.pngimage ,options.left_start,options.hspread,options.lane_vert_pos,options.numlanes,options.lanenames.split(","),options.flip,options.show_grid,options.bench_mark_x,options.fudge,options.shift_marker, options.scale_marker)
     except Exception as e:
         print e
         p.print_help()
